d02f6f6d1cb9e77bb6b1b85a168c8a9a8e3621e7
luoxfish <2108268486@qq.com>
2026-03-04 01:37:41 +0800

    Site updated: 2026-03-04 01:37

--- fix-twikoo.sh
+++ fix-twikoo.sh
@@ -3,7 +3,7 @@
 KEY="07a525d347bb7247"
 
 echo "📥 下载 twikoo.min.js..."
-curl -o source/js/twikoo.min.js https://cdn.jsdelivr.net/npm/twikoo@1.6.44/dist/twikoo.min.js
+curl -o source/js/twikoo.min.js https://cdn.jsdelivr.net/npm/twikoo@1.7.1/dist/twikoo.min.js
 
 echo "🔧 替换 API 地址..."
 sed -i "s|https://api.qjqq.cn/api/qqinfo?qq=|https://v1.nsuuu.com/api/qqname?key=${KEY}\&qq=|g" source/js/twikoo.min.js
